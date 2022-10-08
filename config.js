dd591cbd9ac2ce35aae7c7e1931ff77f85027677
berlin1111 <115251388+berlin1111@users.noreply.github.com>
2022-10-08 09:15:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "开始啦",
         play: "music",
         bannar_coming: "go on",
         balloons_flying: "好像少点东西",
